cb04a989e1f3a64ac60b9e2f7819964e48490780
Eicy <im@errorlog.ml>
2020-03-14 10:48:25 +0800

    HelloWorld

---
+++ Work/src/E1.java
@@ -0,0 +1,19 @@
+public class E1 {
+    public static void main(String[] args){
+        int money = 1;
+        int day = 0;
+        int all= 0;
+        //while (day < 9){
+           // day++;
+           // money = money*2;
+           // all=all+money;
+           // System.out.println(all);
+        //}
+        for(int e = 0;e<10;e++){
+            money = money *2;
+            all=all+money;
+            System.out.println(all+"z");
+        }
+    }
+}
+

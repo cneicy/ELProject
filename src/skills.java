8ceb114bce4a32a87148a83ccb6b744b2efb79e3
Eicy <32791896+cneicy@users.noreply.github.com>
2020-03-16 20:44:38 +0800

    23333

--- src/skills.java
+++ src/skills.java
@@ -21,7 +21,6 @@ public class skills {
                 System.out.printf("造成了:" + PAttack + "点伤害.");
                 Boss.hp = Boss.hp - PAttack;
             }
-
             public void bossk() {
                 System.out.println("Boss使用了大地震荡，对全部玩家造成" + bosskill + "点伤害.");
                 Player1.hp = Player1.hp - 8;
@@ -29,7 +28,6 @@ public class skills {
                 Player3.hp = Player3.hp - 8;
                 Player4.hp = Player4.hp - 8;
             }
-
             public void bossAa() {
                 System.out.println("Boss使用了普通攻击造成了" + bossA + "点伤害.");
 
